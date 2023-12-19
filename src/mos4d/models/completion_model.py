dd0e96cd94a91a02b0d36a02d1c47aa6501c9d33
ZiliangMiao <miaozl@connect.hku.hk>
2023-12-19 13:58:10 +0800

    backup 1219

---
+++ src/mos4d/models/completion_model.py
@@ -0,0 +1,382 @@
+import os
+import sys
+import subprocess
+import argparse
+import logging
+import numpy as np
+from time import time
+import urllib
+
+# Must be imported before large libs
+import open3d as o3d
+
+import torch
+import torch.nn as nn
+import torch.utils.data
+import torch.optim as optim
+
+import MinkowskiEngine as ME
+
+class CompletionNet(nn.Module):
+
+    ENC_CHANNELS = [16, 32, 64, 128, 256, 512, 1024]
+    DEC_CHANNELS = [16, 32, 64, 128, 256, 512, 1024]
+
+    def __init__(self, resolution, in_nchannel=512):
+        nn.Module.__init__(self)
+
+        self.resolution = resolution
+
+        # Input sparse tensor must have tensor stride 128.
+        enc_ch = self.ENC_CHANNELS  # encoder channels
+        dec_ch = self.DEC_CHANNELS  # decoder channels
+        # [16, 32, 64, 128, 256, 512, 1024]
+
+        # Encoder
+        self.enc_block_s1 = nn.Sequential(
+            ME.MinkowskiConvolution(1, enc_ch[0], kernel_size=3, stride=1, dimension=3),
+            ME.MinkowskiBatchNorm(enc_ch[0]),
+            ME.MinkowskiELU(),
+        )
+
+        self.enc_block_s1s2 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                enc_ch[0], enc_ch[1], kernel_size=2, stride=2, dimension=3
+            ),
+            ME.MinkowskiBatchNorm(enc_ch[1]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(enc_ch[1], enc_ch[1], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(enc_ch[1]),
+            ME.MinkowskiELU(),
+        )
+
+        self.enc_block_s2s4 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                enc_ch[1], enc_ch[2], kernel_size=2, stride=2, dimension=3
+            ),
+            ME.MinkowskiBatchNorm(enc_ch[2]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(enc_ch[2], enc_ch[2], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(enc_ch[2]),
+            ME.MinkowskiELU(),
+        )
+
+        self.enc_block_s4s8 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                enc_ch[2], enc_ch[3], kernel_size=2, stride=2, dimension=3
+            ),
+            ME.MinkowskiBatchNorm(enc_ch[3]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(enc_ch[3], enc_ch[3], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(enc_ch[3]),
+            ME.MinkowskiELU(),
+        )
+
+        self.enc_block_s8s16 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                enc_ch[3], enc_ch[4], kernel_size=2, stride=2, dimension=3
+            ),
+            ME.MinkowskiBatchNorm(enc_ch[4]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(enc_ch[4], enc_ch[4], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(enc_ch[4]),
+            ME.MinkowskiELU(),
+        )
+
+        self.enc_block_s16s32 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                enc_ch[4], enc_ch[5], kernel_size=2, stride=2, dimension=3
+            ),
+            ME.MinkowskiBatchNorm(enc_ch[5]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(enc_ch[5], enc_ch[5], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(enc_ch[5]),
+            ME.MinkowskiELU(),
+        )
+
+        self.enc_block_s32s64 = nn.Sequential(
+            ME.MinkowskiConvolution(
+                enc_ch[5], enc_ch[6], kernel_size=2, stride=2, dimension=3
+            ),
+            ME.MinkowskiBatchNorm(enc_ch[6]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(enc_ch[6], enc_ch[6], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(enc_ch[6]),
+            ME.MinkowskiELU(),
+        )
+
+        # Decoder
+        self.dec_block_s64s32 = nn.Sequential(
+            ME.MinkowskiGenerativeConvolutionTranspose(
+                enc_ch[6],
+                dec_ch[5],
+                kernel_size=4,
+                stride=2,
+                dimension=3,
+            ),
+            ME.MinkowskiBatchNorm(dec_ch[5]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(dec_ch[5], dec_ch[5], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(dec_ch[5]),
+            ME.MinkowskiELU(),
+        )
+
+        self.dec_s32_cls = ME.MinkowskiConvolution(
+            dec_ch[5], 1, kernel_size=1, bias=True, dimension=3
+        )
+
+        self.dec_block_s32s16 = nn.Sequential(
+            ME.MinkowskiGenerativeConvolutionTranspose(
+                enc_ch[5],
+                dec_ch[4],
+                kernel_size=2,
+                stride=2,
+                dimension=3,
+            ),
+            ME.MinkowskiBatchNorm(dec_ch[4]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(dec_ch[4], dec_ch[4], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(dec_ch[4]),
+            ME.MinkowskiELU(),
+        )
+
+        self.dec_s16_cls = ME.MinkowskiConvolution(
+            dec_ch[4], 1, kernel_size=1, bias=True, dimension=3
+        )
+
+        self.dec_block_s16s8 = nn.Sequential(
+            ME.MinkowskiGenerativeConvolutionTranspose(
+                dec_ch[4],
+                dec_ch[3],
+                kernel_size=2,
+                stride=2,
+                dimension=3,
+            ),
+            ME.MinkowskiBatchNorm(dec_ch[3]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(dec_ch[3], dec_ch[3], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(dec_ch[3]),
+            ME.MinkowskiELU(),
+        )
+
+        self.dec_s8_cls = ME.MinkowskiConvolution(
+            dec_ch[3], 1, kernel_size=1, bias=True, dimension=3
+        )
+
+        self.dec_block_s8s4 = nn.Sequential(
+            ME.MinkowskiGenerativeConvolutionTranspose(
+                dec_ch[3],
+                dec_ch[2],
+                kernel_size=2,
+                stride=2,
+                dimension=3,
+            ),
+            ME.MinkowskiBatchNorm(dec_ch[2]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(dec_ch[2], dec_ch[2], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(dec_ch[2]),
+            ME.MinkowskiELU(),
+        )
+
+        self.dec_s4_cls = ME.MinkowskiConvolution(
+            dec_ch[2], 1, kernel_size=1, bias=True, dimension=3
+        )
+
+        self.dec_block_s4s2 = nn.Sequential(
+            ME.MinkowskiGenerativeConvolutionTranspose(
+                dec_ch[2],
+                dec_ch[1],
+                kernel_size=2,
+                stride=2,
+                dimension=3,
+            ),
+            ME.MinkowskiBatchNorm(dec_ch[1]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(dec_ch[1], dec_ch[1], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(dec_ch[1]),
+            ME.MinkowskiELU(),
+        )
+
+        self.dec_s2_cls = ME.MinkowskiConvolution(
+            dec_ch[1], 1, kernel_size=1, bias=True, dimension=3
+        )
+
+        self.dec_block_s2s1 = nn.Sequential(
+            ME.MinkowskiGenerativeConvolutionTranspose(
+                dec_ch[1],
+                dec_ch[0],
+                kernel_size=2,
+                stride=2,
+                dimension=3,
+            ),
+            ME.MinkowskiBatchNorm(dec_ch[0]),
+            ME.MinkowskiELU(),
+            ME.MinkowskiConvolution(dec_ch[0], dec_ch[0], kernel_size=3, dimension=3),
+            ME.MinkowskiBatchNorm(dec_ch[0]),
+            ME.MinkowskiELU(),
+        )
+
+        self.dec_s1_cls = ME.MinkowskiConvolution(
+            dec_ch[0], 1, kernel_size=1, bias=True, dimension=3
+        )
+
+        # pruning
+        self.pruning = ME.MinkowskiPruning()
+
+    def get_target(self, out, target_key, kernel_size=1):
+        with torch.no_grad():
+            target = torch.zeros(len(out), dtype=torch.bool, device=out.device)
+            cm = out.coordinate_manager
+            strided_target_key = cm.stride(
+                target_key, out.tensor_stride[0],
+            )
+            kernel_map = cm.kernel_map(
+                out.coordinate_map_key,
+                strided_target_key,
+                kernel_size=kernel_size,
+                region_type=1,
+            )
+            for k, curr_in in kernel_map.items():
+                target[curr_in[0].long()] = 1
+        return target
+
+    def valid_batch_map(self, batch_map):
+        for b in batch_map:
+            if len(b) == 0:
+                return False
+        return True
+
+    def forward(self, partial_in, target_key):
+        out_cls, targets = [], []
+
+        enc_s1 = self.enc_block_s1(partial_in)
+        enc_s2 = self.enc_block_s1s2(enc_s1)
+        enc_s4 = self.enc_block_s2s4(enc_s2)
+        enc_s8 = self.enc_block_s4s8(enc_s4)
+        enc_s16 = self.enc_block_s8s16(enc_s8)
+        enc_s32 = self.enc_block_s16s32(enc_s16)
+        enc_s64 = self.enc_block_s32s64(enc_s32)
+
+        ##################################################
+        # Decoder 64 -> 32
+        ##################################################
+        dec_s32 = self.dec_block_s64s32(enc_s64)
+
+        # Add encoder features
+        dec_s32 = dec_s32 + enc_s32
+        dec_s32_cls = self.dec_s32_cls(dec_s32)
+        keep_s32 = (dec_s32_cls.F > 0).squeeze()
+
+        target = self.get_target(dec_s32, target_key)
+        targets.append(target)
+        out_cls.append(dec_s32_cls)
+
+        if self.training:
+            keep_s32 += target
+
+        # Remove voxels s32
+        dec_s32 = self.pruning(dec_s32, keep_s32)
+
+        ##################################################
+        # Decoder 32 -> 16
+        ##################################################
+        dec_s16 = self.dec_block_s32s16(dec_s32)
+
+        # Add encoder features
+        dec_s16 = dec_s16 + enc_s16
+        dec_s16_cls = self.dec_s16_cls(dec_s16)
+        keep_s16 = (dec_s16_cls.F > 0).squeeze()
+
+        target = self.get_target(dec_s16, target_key)
+        targets.append(target)
+        out_cls.append(dec_s16_cls)
+
+        if self.training:
+            keep_s16 += target
+
+        # Remove voxels s16
+        dec_s16 = self.pruning(dec_s16, keep_s16)
+
+        ##################################################
+        # Decoder 16 -> 8
+        ##################################################
+        dec_s8 = self.dec_block_s16s8(dec_s16)
+
+        # Add encoder features
+        dec_s8 = dec_s8 + enc_s8
+        dec_s8_cls = self.dec_s8_cls(dec_s8)
+
+        target = self.get_target(dec_s8, target_key)
+        targets.append(target)
+        out_cls.append(dec_s8_cls)
+        keep_s8 = (dec_s8_cls.F > 0).squeeze()
+
+        if self.training:
+            keep_s8 += target
+
+        # Remove voxels s16
+        dec_s8 = self.pruning(dec_s8, keep_s8)
+
+        ##################################################
+        # Decoder 8 -> 4
+        ##################################################
+        dec_s4 = self.dec_block_s8s4(dec_s8)
+
+        # Add encoder features
+        dec_s4 = dec_s4 + enc_s4
+        dec_s4_cls = self.dec_s4_cls(dec_s4)
+
+        target = self.get_target(dec_s4, target_key)
+        targets.append(target)
+        out_cls.append(dec_s4_cls)
+        keep_s4 = (dec_s4_cls.F > 0).squeeze()
+
+        if self.training:
+            keep_s4 += target
+
+        # Remove voxels s4
+        dec_s4 = self.pruning(dec_s4, keep_s4)
+
+        ##################################################
+        # Decoder 4 -> 2
+        ##################################################
+        dec_s2 = self.dec_block_s4s2(dec_s4)
+
+        # Add encoder features
+        dec_s2 = dec_s2 + enc_s2
+        dec_s2_cls = self.dec_s2_cls(dec_s2)
+
+        target = self.get_target(dec_s2, target_key)
+        targets.append(target)
+        out_cls.append(dec_s2_cls)
+        keep_s2 = (dec_s2_cls.F > 0).squeeze()
+
+        if self.training:
+            keep_s2 += target
+
+        # Remove voxels s2
+        dec_s2 = self.pruning(dec_s2, keep_s2)
+
+        ##################################################
+        # Decoder 2 -> 1
+        ##################################################
+        dec_s1 = self.dec_block_s2s1(dec_s2)
+        dec_s1_cls = self.dec_s1_cls(dec_s1)
+
+        # Add encoder features
+        dec_s1 = dec_s1 + enc_s1
+        dec_s1_cls = self.dec_s1_cls(dec_s1)
+
+        target = self.get_target(dec_s1, target_key)
+        targets.append(target)
+        out_cls.append(dec_s1_cls)
+        keep_s1 = (dec_s1_cls.F > 0).squeeze()
+
+        # Last layer does not require adding the target
+        # if self.training:
+        #     keep_s1 += target
+
+        # Remove voxels s1
+        dec_s1 = self.pruning(dec_s1, keep_s1)
+
+        return out_cls, targets, dec_s1
